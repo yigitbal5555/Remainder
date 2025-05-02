1d4ba91f0924b39e8b458f97c1b9ecfeb7b9c677
Yiğit BAL <128126749+yigitbal5555@users.noreply.github.com>
2025-05-03 00:30:42 +0300

    commit.
    commit.

---
+++ Remainderr/Remainderr/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Remainderr
+//
+//  Created by Yiğit Bal on 2.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        newTaskButton()
+    }
+    
+    
+    func newTaskButton() {
+        
+        let newTaskButton = UIButton(type: .system)
+        newTaskButton.setTitle("New Task", for: .normal)
+        newTaskButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        newTaskButton.setTitleColor( .white, for: .normal)
+        newTaskButton.backgroundColor = .systemBlue
+        newTaskButton.layer.cornerRadius = 10
+        newTaskButton.translatesAutoresizingMaskIntoConstraints = false
+        newTaskButton.addTarget(self, action: #selector(newTaskButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(newTaskButton)
+        
+        NSLayoutConstraint.activate([
+            newTaskButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            newTaskButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            newTaskButton.widthAnchor.constraint(equalToConstant: 100),
+            newTaskButton.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        }
+        
+    @objc func newTaskButtonTapped() {
+        
+    }
+}
